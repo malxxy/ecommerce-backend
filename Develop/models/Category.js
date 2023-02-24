c560ed6b3b02948e7a3eb6cf69119e45688df29d
malxxy <maliayraguen@gmail.com>
2023-02-24 14:11:45 -0800

    category init

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,32 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // product_name: 'Plain T-Shirt',
+    // price: 14.99,
+    // stock: 14,
+    // category_id: 1,
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
   {
     sequelize,
