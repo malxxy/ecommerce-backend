f0b4bd5cb4840d2e559b4909fd037859c0b6a69d
malxxy <maliayraguen@gmail.com>
2023-02-24 14:31:06 -0800

    belongsTo, hasMany, syntac for models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 
 // Categories have many Products
+Categrory.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
